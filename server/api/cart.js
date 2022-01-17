63ac6df64fa279e36d4a29a79ec41df79ef53312
Ashley Tran <ash@Ashleys-MacBook-Air.local>
2022-01-17 16:50:30 -0500

    get route created for cart to view user's pending order, association added to index

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,12 +1,33 @@
 const router = require("express").Router();
+const { redirect } = require("express/lib/response");
 const ItemizedOrder = require("../db/models/ItemizedOrder");
+const Order = require("../db/models/Order");
 
 router.get("/", async (req, res, next) => {
   try {
-    res.json();
+    const userOrder = await Order.findAll({
+        include: {
+            model: ItemizedOrder,
+            as: "itemizedOrders"
+        },
+        where: {
+            userId: req.body.userId, //this needs to change once we can get the JWT token from authentication header
+            checkOut: false
+        }
+    });  
+    res.json(userOrder[0]);
   } catch (error) {
     next(error);
   }
 });
 
+// router.put("/:id", async (req, res, next) => {
+//     try {
+
+//     }
+//     catch (error) {
+//         next(error);
+//     }
+// })
 module.exports = router;
+

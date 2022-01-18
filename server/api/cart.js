484f51f89801a26116579701dd41dfdb3dd4885a
Ashley Tran <ash@ashleys-air.myfiosgateway.com>
2022-01-17 22:32:42 -0500

    post route created for cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,33 +2,58 @@ const router = require("express").Router();
 const { redirect } = require("express/lib/response");
 const ItemizedOrder = require("../db/models/ItemizedOrder");
 const Order = require("../db/models/Order");
+const Superhero = require("../db/models/superhero");
 
 //getting the current cart with list of cart items (itemizedOrders)
 router.get("/", async (req, res, next) => {
   try {
     const userOrder = await Order.findAll({ 
+      //eager loading to include the itemizedOrders 
         include: {
-            model: ItemizedOrder, //eager loading to include the itemizedOrders 
+            model: ItemizedOrder,
             as: "itemizedOrders"
         },
         where: {
-            userId: req.body.userId, //this needs to change once we can get the JWT token from authentication header
+          //this needs to change once we can get the JWT token from authentication header
+            userId: req.body.userId,
             checkOut: false
         }
     });  
-    res.json(userOrder[0]); //findAll should return an array of one item (each use can only have one "checkedout:false" cart at a time)
+    //findAll should return an array of one item (each use can only have one "checkedout:false" cart at a time)
+    res.json(userOrder[0]); 
   } catch (error) {
     next(error);
   }
 });
 
+//add item to cart
+router.post("/", async (req, res, next) => {
+  try {
+    //finding the open cart that matches up to the userID (we will need to change this when we get the JWT token) 
+    const cart = Order.findOne({ where: {userId: req.body.userId, checkOut: false}}); 
+    //finding the right superhero
+    const hero = Superhero.findByPk(req.body.superheroId);  
+    //creating the new ItemizedOrder instance (new cart item)
+    const newCartItem = await ItemizedOrder.create({
+      days: req.body.days, subtotal: req.body.days * hero.cost, orderId: cart.id, superheroId: hero.id
+    }); 
+    res.status(200).send(newCartItem)
+  }
+  catch (error) {
+    next(error);
+  }
+})
+
 //deleting an item from the cart
 router.delete("/:id", async (req, res, next) => {
   try {
-    const cartItemToDelete = await ItemizedOrder.findByPk(req.params.id); //finds the cart item by the ID
+    //finds the cart item by the ID
+    const cartItemToDelete = await ItemizedOrder.findByPk(req.params.id);
     const orderId = cartItemToDelete.orderId; 
-    await cartItemToDelete.destroy(); //delete item 
-    await updateOrder(orderId); //calling my helper function to update the cart totalDays
+    //delete item 
+    await cartItemToDelete.destroy();
+    //calling my helper function to update the cart totalDays
+    await updateOrder(orderId);
     res.status(204).send();
   }
   catch (error) {
@@ -39,11 +64,14 @@ router.delete("/:id", async (req, res, next) => {
 //update item (num of days booked) in cart
 router.patch("/:id", async (req, res, next) => {
     try {
-        const updatedCartItem = await ItemizedOrder.findByPk(req.params.id); //finds the cart item by the ID
-        await updatedCartItem.update({ //updates the cart item days 
+      //finds the cart item by the ID
+        const updatedCartItem = await ItemizedOrder.findByPk(req.params.id); 
+        //updates the cart item days 
+        await updatedCartItem.update({
           days: req.body.days
         });
-        await updateOrder(updatedCartItem.orderId); //calling my helper function to update the cart totalDays
+        //calling my helper function to update the cart totalDays
+        await updateOrder(updatedCartItem.orderId);
         res.status(200).send(updatedCartItem);
     }
     catch (error) {

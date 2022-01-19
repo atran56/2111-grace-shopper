e43693ad1c4311e6c278d9180dac8258309d5034
Soli Choi <91699214+solichoi@users.noreply.github.com>
2022-01-19 16:58:07 -0500

    Update User.js

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -55,7 +55,7 @@ User.findByToken = async function (token) {
   try {
     const { id } = await jwt.verify(token, process.env.JWT);
     const user = User.findByPk(id);
-    console.log("User Model findbyToken user:", user);
+ 
     if (!user) {
       throw "nooo";
     }

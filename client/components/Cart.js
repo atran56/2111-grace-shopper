3c0c49fb3c31a76114d3bf26f0cd455aa2755c6d
Ashley Tran <ash@Ashleys-MacBook-Air.local>
2022-01-18 20:20:53 -0500

    cart page rendering with userId: 1's cart info

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { fetchCart } from "../store/Cart";
-import { fetchSuperhero } from "../store/singleSuperhero";
+import { fetchSuperheroes } from "../store/superheroes";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-//itemizedOrders and superheroes to populate cart
-const userId = {
-  userId: 2
-}
 class Cart extends React.Component {
   componentDidMount() {
-    this.props.fetchCart(userId)
+    this.props.fetchCart()
+    this.props.fetchSuperheroes()
   }
   render() {
-    //const orderTotal = this.state.cart.reduce((acc, currVal) => acc.subtotal + currVal.subtotal)
-    console.log("HERE!!!!", this.props.cart)
+    if (this.props.cart.loading) {
+      return (<p>Data is loading...</p>)
+    }
+    const subTotal = this.props.cart.cart.itemizedOrders.reduce((acc, currVal) => acc.subtotal + currVal.subtotal)
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            {(this.props.cart.itemizedOrders.length <= 1) ? <h4>{this.props.cart.itemizedOrders.length} Superhero in your cart</h4>
-            : <h4>{this.props.cart.itemizedOrders.length} Superheroes in your cart</h4>}
+            {(this.props.cart.cart.itemizedOrders.length <= 1) ? <h4>{this.props.cart.cart.itemizedOrders.length} Superhero in your cart</h4>
+            : <h4>{this.props.cart.cart.itemizedOrders.length} Superheroes in your cart</h4>}
           </div>
         </div>
           <div className="cart-table"> 
@@ -30,29 +29,32 @@ class Cart extends React.Component {
                   <th scope="col" className="col-sm-4">Superhero</th>
                   <th scope="col" className="col-sm-3">Price per Day</th>
                   <th scope="col" className="col-sm-3">Number of Days Booked</th>
-                  <th scope="col" className="col-sm-2">Total</th>
+                  <th scope="col" className="col-sm-1">Total</th>
+                  <th scope="col" className="col-sm-1">Remove</th>
                 </tr>
               </thead>
               <tbody>
-                {this.props.cart.itemizedOrders.map(item => (
+                {this.props.cart.cart.itemizedOrders.map(item => (
                   <tr>
                     <th scope="row">
-                      <img src={this.props.superhero[item.superheroId].image} style={{ width: '150px', height: '150px', borderRadius: '50%'}}/>
+                      <img src={this.props.superheroes.filter(hero => {return (hero.id === item.superheroId)})[0].image} style={{ width: '150px', height: '150px', borderRadius: '50%'}}/>
                     </th>
-                    <td>${this.props.superhero[item.superheroId].cost}</td>
+                    <td>${this.props.superheroes.filter(hero => {return (hero.id === item.superheroId)})[0].cost}</td>
                     <td>
                     <div className="counter">
                     <input width="50px" type="number" className="form-control" id="input" value={item.days} min="0" max="14"/>
                     </div>
                   </td>
                   <td>${item.subtotal}</td>
+                  <td><button type="button" class="btn btn-link">remove</button></td>
                 </tr>
                 ))}
                 <tr>
                   <th scope="row"></th>
                   <td></td>
                   <td></td>
-                  <td>SUBTOTAL: $100</td>
+                  <td></td>
+                  <td>SUBTOTAL: ${subTotal}</td>
                 </tr>
               </tbody>
             </table>
@@ -63,7 +65,7 @@ class Cart extends React.Component {
               <button type="button" className="btn btn-success">Checkout</button>
             </div>
             <div className="float-start">
-              <Link to="/all">
+              <Link to="/superheroes">
               <button type="button" className="btn btn-light">Continue Shopping</button>
               </Link>
             </div>
@@ -74,14 +76,15 @@ class Cart extends React.Component {
   }
 }
 
-const mapState = state => ({
+const mapState = (state) => ({
     cart: state.cart,
-    superhero: state.superhero
+    loading: state.loading,
+    superheroes: state.superheroes
   });
   
-  const mapDispatch = dispatch => ({
-    fetchCart: (userId) => dispatch(fetchCart(userId)),
-    fetchSuperhero: () => dispatch(fetchSuperhero(superheroId))
+  const mapDispatch = (dispatch) => ({
+    fetchCart: () => dispatch(fetchCart()),
+    fetchSuperheroes: () => dispatch(fetchSuperheroes())
   });
   
   export default connect(mapState, mapDispatch)(Cart);
